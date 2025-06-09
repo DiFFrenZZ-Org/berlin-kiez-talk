116793c1d554721a15a23c5b2010d5f8f6094b0e
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2025-06-09 15:14:15 +0200

    Console log. Test

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,7 +5,10 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error("Supabase env vars are not set. Check .env file.");
+}
+
+
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
