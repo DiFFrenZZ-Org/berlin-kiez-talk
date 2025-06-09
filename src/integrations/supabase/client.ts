b0e70fa5cdf204424983e1fc94b7d08c9491584e
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2025-06-09 18:08:25 +0200

    Correction

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,7 +5,7 @@ import type { Database } from './types';
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+if (!SUPABASE_URL || !SUPABASE_PUBLISHABLE_KEY) {
   throw new Error("Supabase env vars are not set. Check .env file.");
 }
 
