6388c65ab2088c06d2ffe78d1b6fa44a2f23d1eb
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2025-06-12 17:19:22 +0200

    Add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,29 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     server: {
-      host: "::",
+      host: '::',
       port: 8080,
     },
     plugins: [
+      tailwindcss(),
       react(),
-      mode === "development" && componentTagger(),
+      mode === 'development' && componentTagger(),
     ].filter(Boolean),
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     define: {
-      "process.env": env,
+      'process.env': env,
     },
   };
 });
